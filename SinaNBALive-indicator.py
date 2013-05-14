68edb9dfe9d3a30dd394f596836cd7c1de2b0df4
qjp <qjp-ch-mail@163.com>
2013-05-15 00:05:09 +0800

    Finish version 0.1

--- SinaNBALive-indicator.py
+++ SinaNBALive-indicator.py
@@ -3,12 +3,21 @@
 # Author: qjp
 # Date: <2013-05-14 Tue>
 
+import os
 import sys
 import urllib2
 import webbrowser
+import tempfile
+import Image
 from datetime import datetime
-from gi.repository import Gtk, GLib
+from gi.repository import Gtk, GLib, Notify
 from gi.repository import AppIndicator3 as appindicator
+
+app_dir = os.path.dirname(os.path.abspath(__file__))
+app_data_dir = os.path.join(app_dir, 'data')
+app_icon_dir = os.path.join(app_data_dir, 'icon')
+app_gif_dir = os.path.join(app_data_dir, 'gif')
+_, temp_image_file = tempfile.mkstemp()
 
 nbalive_url = 'http://nba.sports.sina.com.cn/js/2007live.js'
 video_live_url = 'http://live.video.sina.com.cn/room/nba'
@@ -60,7 +69,7 @@ class PreferenceDialog(Gtk.Dialog):
         self.set_default_size(150, 100)
         hbox = Gtk.Box(spacing=6)
         hbox.pack_start(Gtk.Label('Refresh Interval:'), False, False, 0)
-        adj = Gtk.Adjustment(initval, 1, 100, 1)
+        adj = Gtk.Adjustment(initval, 1, 3600, 1)
         self.spinbutton = Gtk.SpinButton()
         self.spinbutton.set_adjustment(adj)
         hbox.pack_start(self.spinbutton, False, False, 0)
@@ -80,6 +89,7 @@ class GameMenuItem(object):
         self.game_title.show()
         self.game_scores.show()
         self.sep.show()
+        self.prev_status = ''
         # Dynamic part
         self.set_game_menu_items(game)
         
@@ -101,6 +111,14 @@ class GameMenuItem(object):
         self.game = game
         self.set_game_title()
         self.set_game_scores()
+        if self.game.status != self.prev_status:
+            make_temp_image(game.visiting_en, game.home_en)
+            send_notification('%s VS %s' %(self.game.visiting_zh, self.game.home_zh),
+                              '%s : %s<br/>%s' %(self.game.visiting_score,
+                                                 self.game.home_score,
+                                                 self.game.get_game_status_zh()),
+                              temp_image_file)
+            self.prev_status = self.game.status
 
     def open_game_url(self, widget):
         status = self.game.status
@@ -111,27 +129,45 @@ class GameMenuItem(object):
         else:
             webbrowser.open(text_live_url + game.game_id)
             
+def make_temp_image(visiting_en, home_en):
+    visiting_gif = Image.open(os.path.join(app_gif_dir, visiting_en) + '.gif')
+    home_gif = Image.open(os.path.join(app_gif_dir, home_en) + '.gif')
+    vs = Image.open(os.path.join(app_gif_dir, 'vs.gif'))
+    new_im = Image.new('RGB', (80, 80))
+    new_im.paste(visiting_gif, (0, 0))
+    new_im.paste(home_gif, (30, 40))
+    new_im.paste(vs, (25, 30))
+    new_im.save(temp_image_file, format='png')
+            
+def send_notification(title, description, image):
+    Notify.init(title)
+    n = Notify.Notification.new(title, description, image)
+    n.set_hint_string('x-canonical-append','')
+    n.show()
+            
 def get_today_string():
     today = datetime.today()
     return '%d-%2d-%2d' %(today.year, today.month, today.day)
 
 class NBALiveIndicator(object):
     def __init__(self):
-        self.refresh_interval = 2
+        self.need_update = True
+        self.refresh_interval = 60
         self.today_string = '1990-00-00'
-        self.ind = appindicator.Indicator.new(
-            "example-simple-client",
-            "indicator-messages",
-            appindicator.IndicatorCategory.APPLICATION_STATUS)
-        self.ind.set_status (appindicator.IndicatorStatus.ACTIVE)
-        self.ind.set_attention_icon("indicator-messages-new")
+        self.ind = appindicator.Indicator.new_with_path(
+            "Sina-NBALive-indicator",
+            "nbalive-indicator",
+            appindicator.IndicatorCategory.OTHER,
+            app_icon_dir)
+        self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+        self.ind.set_attention_icon("nbalive-indicator-update")
         self.game_menu_item_list = []
         self.menu = Gtk.Menu()
         self.static_menu_item_setup()
         self.ind.set_menu(self.menu)
 
     def static_menu_item_setup(self):
-        self.quit_item = Gtk.MenuItem("Quit")
+        self.quit_item = Gtk.ImageMenuItem.new_from_stock(Gtk.STOCK_QUIT, None)
         self.quit_item.connect('activate', Gtk.main_quit)
         self.quit_item.show()
         self.menu.append(self.quit_item)
@@ -149,7 +185,7 @@ class NBALiveIndicator(object):
 
     def dynamic_menu_item_update(self):
         for game, item in zip(self.games, self.game_menu_item_list):
-            item.set_game_menu_items(game)        
+            item.set_game_menu_items(game)
     
     def show_preference(self,widget):
         dialog = PreferenceDialog(self.refresh_interval)
@@ -157,23 +193,42 @@ class NBALiveIndicator(object):
         if response == Gtk.ResponseType.OK:
             self.refresh_interval = dialog.get_new_value()
             print 'New refresh interval: %ds' %self.refresh_interval
+            if self.timer_id > 0:
+                GLib.source_remove(self.timer_id)
+                self.timer_id = GLib.timeout_add_seconds(self.refresh_interval, self.do_update)
         dialog.destroy()
         
     def main(self):
+        import signal
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
         self.do_update()
-        GLib.timeout_add_seconds(self.refresh_interval, self.do_update)
+        self.timer_id = GLib.timeout_add_seconds(self.refresh_interval, self.do_update)
         Gtk.main()
         
     def do_update(self):
-        print '\033[0;32mupdate...\033[0m'
-        # self.games = get_today_games()
-        # if get_today_string() > self.today_string:
-        #     for i in self.game_menu_item_list:
-        #         for j in i.get_game_menu_items():
-        #             self.menu.remove(j)
-        #     self.dynamic_menu_item_setup()
-        # else:
-        #     self.dynamic_menu_item_update()
-    
+        print '\033[0;32m[Message]: \033[0mupdate'
+        new_today_string = get_today_string()
+        if self.need_update:
+            self.games = get_today_games()
+            if new_today_string > self.today_string:
+                self.today_string = new_today_string
+                for i in self.game_menu_item_list:
+                    for j in i.get_game_menu_items():
+                        self.menu.remove(j)
+                self.dynamic_menu_item_setup()
+            else:
+                self.dynamic_menu_item_update()
+            if len(filter(lambda x: x.status != 'Final', self.games)) == 0:
+                self.need_update = False
+                self.ind.set_status(appindicator.IndicatorStatus.ACTIVE)
+            else:
+                self.ind.set_status(appindicator.IndicatorStatus.ATTENTION)
+        else:
+            if new_today_string > self.today_string:
+                self.today_string = new_today_string
+                self.need_update = True
+                self.do_update()
+        return True
+
 if __name__ == '__main__':
     NBALiveIndicator().main()
